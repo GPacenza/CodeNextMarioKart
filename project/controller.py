b5e59e4eec1eec6f2d474fbdc098af6c6e87f14d
GPacenza <31751381+GPacenza@users.noreply.github.com>
2023-06-14 20:21:36 -0400

    Update controller.py

--- project/controller.py
+++ project/controller.py
@@ -1,24 +1,27 @@
 import pystk
 
 
-def control(aim_point, current_vel):
-    """
-    Set the Action for the low-level controller
-    :param aim_point: Aim point, in screen coordinate frame [-1..1]
-    :param current_vel: Current velocity of the kart
-    :return: a pystk.Action (set acceleration, brake, steer, drift)
-    """
-    action = pystk.Action()
+def control(aim_point, current_vel, steer_gain=2, skid_thresh=0.5, target_vel=25):
+  import numpy as np
+  action = pystk.Action()
 
-    """
-    Your code here
-    Hint: Use action.acceleration (0..1) to change the velocity. Try targeting a target_velocity (e.g. 20).
-    Hint: Use action.brake to True/False to brake (optionally)
-    Hint: Use action.steer to turn the kart towards the aim_point, clip the steer angle to -1..1
-    Hint: You may want to use action.drift=True for wide turns (it will turn faster)
-    """
+  steer_angle = steer_gain * aim_point[0]
 
-    return action
+  # Compute accelerate
+  action.acceleration = 1.0 if current_vel < target_vel else 0.0
+
+  # Compute steering
+  action.steer = np.clip(steer_angle * steer_gain, -1, 1)
+
+  # Compute skidding
+  if abs(steer_angle) > skid_thresh:
+      action.drift = True
+   else:
+      action.drift = False
+
+  action.nitro = True
+
+  return action
 
 def test_controller(pytux, track, verbose):
   import numpy as np
